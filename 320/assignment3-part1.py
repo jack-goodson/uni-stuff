cb8754f9ed6ef529f3e92693706e408beba0d95f
jack-goodson <32623308+jack-goodson@users.noreply.github.com>
2019-12-05 21:53:18 +1300

    Update assignment3-part1.py

--- 320/assignment3-part1.py
+++ 320/assignment3-part1.py
@@ -1,6 +1,19 @@
 # PROBLEM DESCRIPTION
 
-#
+# Determine the maximum number of non-overlapping intervals.
+#   Sample Input:
+#   4
+#   1 3 0 2 3 4 
+#   0 3 1 2 1 3 4 4 
+#   0 2 3 4 5 6 3 6 2 4 
+#   1 1 1 2 1 3 1 4 1 5
+
+#   Sample output 
+
+#   2
+#   2
+#   3
+#   1
 
 
 inp = str(input())   # graph size
