00dde18e82e051196804a67fbd2107a1634acbe3
jack-goodson <32623308+jack-goodson@users.noreply.github.com>
2019-12-05 21:55:40 +1300

    Create assignment3-part3.py

---
+++ 320/assignment3-part3.py
@@ -0,0 +1,33 @@
+
+inp = str(input())   # graph size
+real_results = []
+
+
+for t in range(int(inp)):
+    results = []
+    inpp = str(input())   # vertex info
+    split_inp = [int(x) for x in inpp.split()]
+
+    t_split_inp = []
+    for i in range(0,len(split_inp), 2):
+        t_split_inp.append((tuple([split_inp[i], split_inp[i+1]])))
+    sorted_inp = sorted(t_split_inp, key=lambda y: y[0])
+
+
+    l = sorted_inp[0][0]
+    h = sorted_inp[0][1]
+    inp_alt = sorted_inp[1:]
+    for tup in inp_alt:
+        if tup[0] <= h:
+            h = max(tup[1], h)
+        else:
+            results.append(h-l)
+            l = tup[0]
+            h = tup[1]
+
+    results.append(h-l)
+
+    real_results.append(max(results))
+
+for i in real_results:
+    print(i)
