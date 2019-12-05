af9c5431d8a5c0f299275fb0e5d95622acc2a9a1
jack-goodson <32623308+jack-goodson@users.noreply.github.com>
2019-12-05 21:56:27 +1300

    Update assignment3-part3.py

--- 320/assignment3-part3.py
+++ 320/assignment3-part3.py
@@ -1,3 +1,20 @@
+#   PROBLEM DESCRIPTION
+
+#   Compute the largest contiguous interval obtained by taking a union of some of the input intervals.
+
+#   Sample Input:
+#   4
+#   1 3 0 2 3 4 
+#   0 3 1 2 1 3 4 4 
+#   0 2 3 4 5 6 3 6 2 4 
+#   1 1 1 2 1 3 1 4 1 5
+
+#   Sample output 
+
+#   4
+#   3
+#   6
+#   4
 
 inp = str(input())   # graph size
 real_results = []
